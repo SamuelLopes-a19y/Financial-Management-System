b7329016784baf4895765209d70db054ece404cf
Samuel Patrick <patrick.samuellopes@gmail.com>
2026-01-28 22:09:31 -0300

    Ajustando rotas de usuário e autenticação

--- src/Routes/userRoutes.js
+++ src/Routes/userRoutes.js
@@ -2,7 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 const userController = require('../controllers/userController')
+const authMiddleware = require('../middlewares/authMiddleware')
 
-router.put('/:id', userController.updateUser)
+//router.put('/:id', userController.updateUser)
+
+router.get('/getUser', authMiddleware, userController.getUser);
 
 module.exports = router
